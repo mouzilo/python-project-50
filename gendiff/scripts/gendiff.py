ce0e2c120be088476507029bfd77f59ac92fc52b
mouzilo <6473839@gmail.com>
2023-06-18 16:50:01 +0300

    second commit

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -6,6 +6,7 @@ def main():
         description='Compares two configuration files and shows a difference.')
     parser.add_argument('first_file', type=str)
     parser.add_argument('second_file', type=str)
+    parser.add_argument('--f', '--format',metavar='FORMAT', type=str, help='set format for output')
     args = parser.parse_args()
     return args.first_file
 
